bea9b1cc90ea81d39e36700cecb7ded0256e19bf
Kelleymb <kelleybuccieri@gmail.com>
2020-06-10 14:36:12 -0400

    all articles-service object tests passing

--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -38,8 +38,6 @@ describe(`Articles service object`, function() {
         // After all tests run, let go of the db connection
         after('destroy db connection', () => db.destroy())
 
-        before(() => db('blogful_articles').truncate())
-
         //HAS DATA TESTS
         context(`Given 'blogful_articles' has data`, () => {
             beforeEach(() => {
@@ -56,80 +54,89 @@ describe(`Articles service object`, function() {
                         })))
                     })
             })
-
-        //HAS NO DATA TESTS
-        context(`Given 'blogful_articles' has no data`, () => {
-            
-            it(`getAllArticles() resolves an empty array`, () => {
-                return ArticlesService
-                    .getAllArticles(db)
-                    .then(actual => expect(actual).to.eql([]));
-            })
-            
-            it(`insertArticle() inserts a new article and resolves the new article with an 'id'`, () => {
-                //new article to use as subject for test
-                const newArticle = {
-                    title: 'Test new title',
-                    content: 'Test new content',
-                    date_published: new Date('2020-01-01T00:00:00.000Z'),
-                };
-                return ArticlesService.insertArticle(db, newArticle)
-                    //Update the test to assert that the method resolves the 
-                    //newly created article with an incremented ID. 
-                    //The ID should be '1', as the table is empty
+            //We can add a test for getting a specific article by ID now, 
+        //we'll call the method getById.
+        it(`getById() resolves an article by id from 'blogful_articles' table`, () => {
+            const thirdId = 3
+            const thirdTestArticle = testArticles[thirdId - 1]
+                return ArticlesService.getById(db, thirdId)
                     .then(actual => {
                         expect(actual).to.eql({
-                            id: 1,
-                            title: newArticle.title,
-                            content: newArticle.content,
-                            date_published: newArticle.date_published,
+                            id: thirdId,
+                            title: thirdTestArticle.title,
+                            content: thirdTestArticle.content,
+                            date_published: thirdTestArticle.date_published,
                         })
                     })
-            })
-            
-            //We can add a test for getting a specific article by ID now, 
-            //we'll call the method getById.
-            it(`getById() resolves an article by id from 'blogful_articles' table`, () => {
-                const thirdId = 3
-                const thirdTestArticle = testArticles[thirdId - 1]
-                    return ArticlesService.getById(db, thirdId)
-                        .then(actual => {
-                            expect(actual).to.eql({
-                                id: thirdId,
-                                title: thirdTestArticle.title,
-                                content: thirdTestArticle.content,
-                                date_published: thirdTestArticle.date_published,
-                            })
-                        })
-            })
-            
-            it(`deleteArticle() removes an article by id from 'blogful_articles' table`, () => {
-                const articleId = 3
-                    return ArticlesService.deleteArticle(db, articleId)
-                    .then(() => ArticlesService.getAllArticles(db))
-                    .then(allArticles => {
-                    // copy the test articles array without the "deleted" article
-                    const expected = testArticles.filter(article => article.id !== articleId)
-                        expect(allArticles).to.eql(expected)
+        })
+        
+        it(`deleteArticle() removes an article by id from 'blogful_articles' table`, () => {
+            const articleId = 3
+                return ArticlesService.deleteArticle(db, articleId)
+                .then(() => ArticlesService.getAllArticles(db))
+                .then(allArticles => {
+                // copy the test articles array without the "deleted" article
+                const expected = testArticles.filter(article => article.id !== articleId)
+                    expect(allArticles).to.eql(expected)
+                })
+        })
+        
+        it(`updateArticle() updates an article from the 'blogful_articles' table`, () => {
+            const idOfArticleToUpdate = 3
+            const newArticleData = {
+                title: 'updated title',
+                content: 'updated content',
+                date_published: new Date(),
+            }
+            return ArticlesService.updateArticle(db, idOfArticleToUpdate, newArticleData)
+                .then(() => ArticlesService.getById(db, idOfArticleToUpdate))
+                .then(article => {
+                    expect(article).to.eql({
+                        id: idOfArticleToUpdate,
+                        ...newArticleData,
                     })
-            })
-            
-            it(`updateArticle() updates an article from the 'blogful_articles' table`, () => {
-                const idOfArticleToUpdate = 3
-                const newArticleData = {
-                    title: 'updated title',
-                    content: 'updated content',
-                    date_published: new Date(),
-                }
-                return ArticlesService.updateArticle(db, idOfArticleToUpdate, newArticleData)
-                    .then(() => ArticlesService.getById(db, idOfArticleToUpdate))
-                    .then(article => {
-                        expect(article).to.eql({
-                            id: idOfArticleToUpdate,
-                            ...newArticleData,
-                        })
+                })
+        })
+    })
+})
+
+describe(`Articles service object`, function() {
+    let db;
+
+        before('setup db', () => {
+            db = knex({
+                client: 'pg',
+                connection: process.env.TEST_DB_URL,
+            });
+        });
+    //HAS NO DATA TESTS
+    context(`Given 'blogful_articles' has no data`, () => {
+        
+        it(`getAllArticles() resolves an empty array`, () => {
+            return ArticlesService
+                .getAllArticles(db)
+                .then(actual => expect(actual).to.eql([]));
+        })
+        
+        it(`insertArticle() inserts a new article and resolves the new article with an 'id'`, () => {
+            //new article to use as subject for test
+            const newArticle = {
+                title: 'Test new title',
+                content: 'Test new content',
+                date_published: new Date('2020-01-01T00:00:00.000Z'),
+            };
+            return ArticlesService.insertArticle(db, newArticle)
+                //Update the test to assert that the method resolves the 
+                //newly created article with an incremented ID. 
+                //The ID should be '1', as the table is empty
+                .then(actual => {
+                    expect(actual).to.eql({
+                        id: 1,
+                        title: newArticle.title,
+                        content: newArticle.content,
+                        date_published: newArticle.date_published,
                     })
-            })
+                })
         })
     })
 })
